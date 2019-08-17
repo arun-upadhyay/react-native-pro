64ec850dea7a4380aa85c4d4ee66eb943a948ab5
Arun Upadhyay <arunnnet@gmail.com>
2019-08-17 08:08:22 -0400

    Counter apps moved

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
 import React, {Component} from 'react';
 import {AppRegistry, Image, ScrollView, Button, Text, StyleSheet, View, TouchableOpacity} from 'react-native';
+import Counter from "./src/components/counter";
 
-export default class Bananas extends Component {
-
-    state = {
-        counter: 0
-    }
-    increaseCounter = () => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    }
-    decreaseCounter = () => {
-        this.setState({
-            counter: this.state.counter - 1
-        })
-    }
+export default class Main extends Component {
 
     render() {
         return (
-            <ScrollView>
-                <TouchableOpacity>
-                    <Text onPress={this.increaseCounter} style={{flexDirection: 'row', width: 200}}> Increase</Text>
-                </TouchableOpacity>
-                <Text style={{flexDirection: 'row', width: 100}}> {this.state.counter} </Text>
-                <TouchableOpacity>
-                    <Text onPress={this.decreaseCounter} style={{flexDirection: 'row', width: 200}}> Decrease</Text>
-                </TouchableOpacity>
-            </ScrollView>
+            <Counter/>
         );
     }
 }
 
-AppRegistry.registerComponent('AwesomePro', () => Bananas);
+AppRegistry.registerComponent('AwesomePro', () => Main);
