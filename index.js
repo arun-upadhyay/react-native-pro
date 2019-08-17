e6d09d6db51c15059165f90c74998b7ac1aad5fe
Arun Upadhyay <arunnnet@gmail.com>
2019-08-17 07:10:50 -0400

    Counter increment sample pro in react-native

---
+++ index.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {AppRegistry, Image, ScrollView, Button, Text, StyleSheet, View, TouchableOpacity} from 'react-native';
+
+export default class Bananas extends Component {
+
+    state = {
+        counter: 0
+    }
+    increaseCounter = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+    decreaseCounter = () => {
+        this.setState({
+            counter: this.state.counter - 1
+        })
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <TouchableOpacity>
+                    <Text onPress={this.increaseCounter} style={{flexDirection: 'row', width: 200}}> Increase</Text>
+                </TouchableOpacity>
+                <Text style={{flexDirection: 'row', width: 100}}> {this.state.counter} </Text>
+                <TouchableOpacity>
+                    <Text onPress={this.decreaseCounter} style={{flexDirection: 'row', width: 200}}> Decrease</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        );
+    }
+}
+
+AppRegistry.registerComponent('AwesomePro', () => Bananas);
